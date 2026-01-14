8080ddaa96af1d737aef4318cacbb7eb2e292798
raidmncr <raidmenacer1usa@gmail.com>
2026-01-14 09:55:36 -0600

    this is the pomodoro app and i am trying to learn about git and git hub

---
+++ script.js
@@ -0,0 +1,54 @@
+const bells =  new Audio("./assets/sounds/bell.mp3");
+
+bells.addEventListener("canplaythrough", () => {
+  console.log("Sound loaded!");
+});
+bells.addEventListener("error", () => {
+  console.log("ERROR loading sound!");
+});
+
+const startBtn = document.querySelector(".btn-start");
+
+const session = document.querySelector(".minutes");
+``
+let myInterval;
+
+let state = true;
+
+const appTimer = () => {
+  const sessionAmount = Number.parseInt(session.textContent);
+
+  if (state) {
+    state = false;
+    let totalSeconds = sessionAmount * 60;
+
+    const updateSeconds = () => {
+      const minuteDiv = document.querySelector(".minutes");
+      const secondDiv = document.querySelector(".seconds");
+
+      totalSeconds--;
+
+      let minutesLeft = Math.floor(totalSeconds / 60);
+      let secondsLeft = totalSeconds % 60;
+
+      if (secondsLeft < 10){
+        secondDiv.textContent = "0" + secondsLeft;
+      }else{
+        secondDiv.textContent = secondsLeft;
+      }
+
+      minuteDiv.textContent = `${minutesLeft}`;
+
+      if (minutesLeft == 0 && secondsLeft == 0){
+        bells.play();
+        clearInterval(myInterval);
+      }
+    };
+    myInterval = setInterval(updateSeconds, 1000);
+  } else {
+    alert("Session has already started.");
+  }
+  
+};
+
+startBtn.addEventListener("click", appTimer);
